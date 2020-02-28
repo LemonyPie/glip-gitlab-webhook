f1c985f3fd5f1cde2696c5fda0dbeefc2792720c
Artem Senin <artem.senin@nordigy.ru>
2020-02-28 21:18:14 +0300

    feat(merge-request): use axios to send post request

--- routes/merge-request.js
+++ routes/merge-request.js
@@ -1,44 +1,28 @@
 var express = require('express');
-var url = require('url');
-var querystring = require('querystring');
-var http = require('http');
+var axios = require('axios');
 var router = express.Router();
-var hookUrlRaw = process.env.MERGE_REQUEST_HOOK_URL;
+var hookUrl = process.env.MERGE_REQUEST_HOOK_URL;
 
 router.post('/', function(req, res, next) {
-  const hookUrl = new URL(hookUrlRaw);
   const assigneeName = req.body.object_attributes.assignee.name;
-  const messageBody = (assigneeName ? `@${assigneeName} please` : 'Please ') + `[have a look](${req.body.object_attributes.url})`;
-  const body = querystring.stringify({
+  const message = (assigneeName ? `@${assigneeName} please` : 'Please ') + `[have a look](${req.body.object_attributes.url})`;
+  const body = {
     "activity": "Merge request",
     "icon": req.body.user.avatar_url,
     "title": `${req.body.user.name} submitted a new merge request`,
-    "body": messageBody
-  });
-
-  const options = {
-    hostname: hookUrl.hostname,
-    port: hookUrl.port || 80,
-    path: hookUrl.pathname,
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': Buffer.byteLength(body)
-    }
-  }
-
-  const request = http.request(options, (res) => {
-    console.log(`STATUS: ${res.statusCode}`);
-    res.on('data', (chunk) => {
-      console.log(`BODY: ${chunk}`);
-    });
+    "body": message
+  };
+  
+  axios.post(hookUrl, body)
+  .then((response) => {
+    res.status(200);
+    res.end('Hook send successfully');
   })
-
-  request.on('error', (e) => console.error(`Problem with request to hook: ${e.message}`))
-  request.write(body);
-  request.end()
-  res.status(200);
-  res.end();
+  .catch((error) => {
+    console.error(error);
+    res.status(400)
+    res.end('Hood sending failed');
+  })
 });
 
 module.exports = router;
