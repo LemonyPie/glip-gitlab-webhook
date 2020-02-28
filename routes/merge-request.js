05513ae6439a8b7ddd8a2fcc2815222feb53e8d4
Artem Senin <artem.senin@nordigy.ru>
2020-02-28 21:40:08 +0300

    fix(merge-request): check if fileds in body are emplty

--- routes/merge-request.js
+++ routes/merge-request.js
@@ -4,12 +4,13 @@ var router = express.Router();
 var hookUrl = process.env.MERGE_REQUEST_HOOK_URL;
 
 router.post('/', function(req, res, next) {
-  const assigneeName = req.body.object_attributes.assignee.name;
+  const assigneeName = req.body.object_attributes && req.body.object_attributes.assignee && req.body.object_attributes.name || '';
   const message = (assigneeName ? `@${assigneeName} please` : 'Please') + ` [have a look](${req.body.object_attributes.url})`;
+  const user = req.body.user;
   const body = {
     "activity": "Merge request",
-    "icon": req.body.user.avatar_url,
-    "title": `${req.body.user.name} submitted a new merge request`,
+    "icon": user && user.avatar_url || '', 
+    "title": `${user && user.name || 'Someone'} submitted a new merge request`,
     "body": message
   };
   
