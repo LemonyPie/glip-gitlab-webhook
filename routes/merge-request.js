b95f8a3fd2461f9f4d34995ceaf79c9557db0445
Artem Senin <artem.senin@nordigy.ru>
2020-02-29 01:59:13 +0300

    feat(merge-request): filter updates exept creation and WIP resolve

--- routes/merge-request.js
+++ routes/merge-request.js
@@ -1,42 +1,60 @@
-var express = require('express');
-var axios = require('axios');
-var router = express.Router();
-var hookUrl = process.env.MERGE_REQUEST_HOOK_URL;
+const express = require('express');
+const axios = require('axios');
+const config = require('../config.json');
+const router = express.Router();
+const hookUrl = process.env.MERGE_REQUEST_HOOK_URL;
 
 router.post('/', function(req, res, next) {
-  console.log('REQUEST BODY: ', JSON.stringify(req.body))
-  const assigneeName = req.body.object_attributes.assignees && req.body.object_attributes.assignees[0] && req.body.object_attributes.assignees[0].name || '';
-  const message = (assigneeName ? `@${assigneeName} please` : 'Please') + ` [have a look](${req.body.object_attributes.url})`;
-  const user = req.body.user;
-  const body = {
-    "activity": "Merge request",
-    "icon": user && user.avatar_url || '', 
-    "body": `${user && user.name || 'Someone'} submitted a new merge request.`,
-    "attachments":[
-      {
-       "type":"Card",
-       "fallback": `[Merge request](${req.body.object_attributes.url})`,
-       "color":"#fc9403",
-       "fields":[
+  const isNewRequest = req.body.object_attributes.action !== 'update';
+  const isResolveWIPStatus = !isNewRequest && 
+    !req.body.object_attributes.work_in_progress &&
+    (req.body.changes && req.body.changes.title && req.body.changes.title.previous.includes('WIP'));
+  console.log('REQUEST BODY: ', JSON.stringify(req.body), `Is new: ${isNewRequest}`, `Is resolve WIP status: ${isResolveWIPStatus}`)
+  if(isNewRequest || isResolveWIPStatus) {
+    const assigneeName = req.body.assignees && req.body.assignees[0] && req.body.assignees[0].name || '';
+    const message = (assigneeName ? `@${assigneeName} please` : 'Please') + ` [have a look](${req.body.object_attributes.url})`;
+    const user = req.body.user;
+    const body = {
+      "activity": isNewRequest ? 'Merge Request Created' : isResolveWIPStatus ? 'Resolved WIP Status' : 'Merge Request Changed',
+      "icon": user && user.avatar_url || '', 
+      "body": `${user && user.name || 'Someone'} submitted a new merge request.`,
+      "attachments":[
         {
-          "title": `${req.body.object_attributes.title}`,
-          "value": message,
-          "style":"Long"
-        }]
-      }
-    ]
-  };
+        "type":"Card",
+        "fallback": `[Merge request](${req.body.object_attributes.url})`,
+        "color": isNewRequest ? config.colors.new : config.colors.update,
+        "fields":[
+            {
+              "title": `${req.body.object_attributes.title}`,
+              "value": message,
+              "style":"Long"
+            },
+            {
+              "title": "Status",
+              "value": req.body.object_attributes.status,
+              "style": "Short"
+            },
+            {
+              "title": "Target branch",
+              "value": req.body.object_attributes.target_branch,
+              "style": "Short"
+            }
+          ]
+        }
+      ]
+    }
   
-  axios.post(hookUrl, body)
-  .then((response) => {
-    res.status(200);
-    res.end('Hook send successfully');
-  })
-  .catch((error) => {
-    console.error(error);
-    res.status(400)
-    res.end('Hood sending failed');
-  })
+    axios.post(hookUrl, body)
+    .then((response) => {
+      res.status(200);
+      res.end('Hook send successfully');
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400)
+      res.end('Hood sending failed');
+    })
+  }
 });
 
 module.exports = router;
